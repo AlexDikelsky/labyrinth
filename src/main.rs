f28aa8c23dddce6df75e06c401cff4d6fc3e17f4
AlexDikelsky <alexander.dikelsky@gmail.com>
2020-01-11 11:39:11 -0600

    Changed booleans to enums so things other than walls and not walls can be added

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,28 @@ use std::fs;
 //This assumes only squares will be used, but I think thats fine
 //These are for how many chars over you can see when going thourgh
 //the maze
-const X_SIGHT: usize = 8;
-const Y_SIGHT: usize = 7;
+const X_SIGHT: usize = 1;
+const Y_SIGHT: usize = 1;
 
-const WALL_CHAR: char = 'X';
-const OPEN_CHAR: char = '.';
+//const WALL_CHAR: char = 'X';
+//const OPEN_CHAR: char = '.';
+
+#[derive(Clone, Copy)]
+enum Terrain {
+    Wall,
+    Open,
+    Theseus,
+    Minotaur,
+}
+
+fn get_terr_char(t: &Terrain) -> char {
+    match t {
+        Terrain::Wall => 'X',
+        Terrain::Open => '.',
+        Terrain::Theseus => '$',
+        Terrain::Minotaur => '?',
+    }
+}
 
 fn main() {
     let filename = "testmaze.txt";
@@ -28,8 +45,8 @@ fn main() {
 
 }
 
-fn parse_string_maze(maze_raw: String) -> Vec<Vec<bool>> {
-    let mut vec_maze: Vec<Vec<bool>> = vec![vec![]];
+fn parse_string_maze(maze_raw: String) -> Vec<Vec<Terrain>> {
+    let mut vec_maze: Vec<Vec<Terrain>> = vec![vec![]];
     let mut row = 0;
 
     for byte_char in maze_raw.bytes() {
@@ -39,10 +56,10 @@ fn parse_string_maze(maze_raw: String) -> Vec<Vec<bool>> {
             10 => {row += 1; vec_maze.push(vec![])},
 
             //X
-            88 => vec_maze[row].push(true),
+            88 => vec_maze[row].push(Terrain::Wall),
 
             //O
-            79 => vec_maze[row].push(false),
+            79 => vec_maze[row].push(Terrain::Open),
             _  => panic!("Invalid Character {}", byte_char),
         }
     }
@@ -50,38 +67,39 @@ fn parse_string_maze(maze_raw: String) -> Vec<Vec<bool>> {
     return vec_maze
 }
 
-fn get_around(x_location: usize, y_location: usize, bool_maze: Vec<Vec<bool>>) -> [[bool; X_SIGHT * 2+1]; Y_SIGHT * 2 + 1] {
-    let mut result = [[true; X_SIGHT * 2 + 1]; Y_SIGHT * 2 + 1];
+fn get_around(x_location: usize, y_location: usize, terr_maze: Vec<Vec<Terrain>>) -> [[Terrain; X_SIGHT * 2+1]; Y_SIGHT * 2 + 1] {
+    let mut result = [[Terrain::Wall; X_SIGHT * 2 + 1]; Y_SIGHT * 2 + 1];
 
     for x in 0..(Y_SIGHT * 2 + 1) {
         for y in 0..(X_SIGHT * 2 + 1) {
             //println!("{} - {}, {} - {}", x, x_location, y, y_location);
-            //println!("{}", x_location + x - X_SIGHT < bool_maze.len()); 
+            //println!("{}", x_location + x - X_SIGHT < terr_maze.len()); 
             if (
                 (((x_location+x).checked_sub(X_SIGHT) != None) && ((y_location+y).checked_sub(Y_SIGHT) != None)) &&
-                ((x_location + x - X_SIGHT < bool_maze.len()) &&  
-                  y_location + y - Y_SIGHT < bool_maze[x].len()) 
+                ((x_location + x - X_SIGHT < terr_maze.len()) &&  
+                  y_location + y - Y_SIGHT < terr_maze[x].len()) 
                 )
                 {
-                    result[x][y] = bool_maze[x_location + x - X_SIGHT][y_location + y - Y_SIGHT];
+                    result[x][y] = terr_maze[x_location + x - X_SIGHT][y_location + y - Y_SIGHT];
             }
         }
     }
     return result
 }
 
-fn as_maze_vec(maze: Vec<Vec<bool>>) -> () {
+//I know I should use generics for these, but I'm not sure how
+fn as_maze_vec(maze: Vec<Vec<Terrain>>) -> () {
     for x in maze.iter() {
         for y in x.iter() {
-            print!("{}", match y {true => WALL_CHAR, false => OPEN_CHAR});
+            print!("{}", get_terr_char(y));
         }
         println!();
     }
 }
-fn as_maze(maze: [[bool; X_SIGHT * 2+1]; Y_SIGHT * 2 + 1]) -> () {
+fn as_maze(maze: [[Terrain; X_SIGHT * 2+1]; Y_SIGHT * 2 + 1]) -> () {
     for x in maze.iter() {
         for y in x.iter() {
-            print!("{}", match y {true => WALL_CHAR, false => OPEN_CHAR});
+            print!("{}", get_terr_char(y));
         }
         println!();
     }
